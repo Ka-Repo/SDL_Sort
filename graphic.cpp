ab72fcf48c377fc41bf5da8a6cd566427d34362c
ka-repo <kariem.abouelhassan@web.de>
2020-11-23 12:27:59 +0100

    extract graphic.cpp content from graphic.h

--- graphic.cpp
+++ graphic.cpp
@@ -1,5 +1,36 @@
 #include "graphic.h"
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 graphic::graphic()
 {
+	window = NULL;
+	screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		window = SDL_CreateWindow("Sorting with SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+		}
+	}
+}
+
+graphic::~graphic()
+{
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
